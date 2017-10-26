9f8e214fba85014a681b4766be1ec6741bcce0de
ChanorJia <jiaxiaocheng6690@gmail.com>
2017-10-26 12:54:33 +0800

    修改leet4的算法

--- Program.cs
+++ Program.cs
@@ -6,12 +6,23 @@ namespace leetcode
     {
         static void Main(string[] args)
         {
-            object ss=1%2;
+            longsetPalindromicSub ss=new longsetPalindromicSub();
+            Console.WriteLine(ss.LongestPalindrome("babad"));  //bab
+            Console.WriteLine(ss.LongestPalindrome("cbbd"));   //bb
+            Console.WriteLine(ss.LongestPalindrome("a"));     //a
+            Console.WriteLine(ss.LongestPalindrome("ccc"));   //ccc
+            Console.WriteLine(ss.LongestPalindrome("bb"));    ///bb
+            Console.WriteLine(ss.LongestPalindrome("abcba"));  //abcba
+            Console.WriteLine(ss.LongestPalindrome("babadada"));   //"adada"
+            Console.WriteLine(ss.LongestPalindrome("sooos"));
             // test test=new test();
             // test.aaa();
-            Console.WriteLine("Hello World!");
-            TwoSumSolution twoSumSolution=new TwoSumSolution();
-            Console.WriteLine(twoSumSolution.TwoSum(new int[]{2,3,5,7,7},6));
+
+
+
+            // Console.WriteLine("Hello World!");
+            // TwoSumSolution twoSumSolution=new TwoSumSolution();
+            // Console.WriteLine(twoSumSolution.TwoSum(new int[]{2,3,5,7,7},6));
         }
     }
 }
--- leetcodeProblems/LongestPalindromicSubstring.cs
+++ leetcodeProblems/LongestPalindromicSubstring.cs
@@ -19,7 +19,72 @@ public class longsetPalindromicSub
 {
     public string LongestPalindrome(string s)
     {
+        Dictionary<string,int> letterDic=new Dictionary<string, int>();
         
-        return "";
+        int startIndx=0;
+        int len=1;
+        
+        for(int i=0;i<s.Length;i++)
+        {
+            string curretnLetter=s.Substring(i,1);
+            if(letterDic.ContainsKey(curretnLetter))
+            {
+                int repeatIndx=-1;
+                letterDic.TryGetValue(curretnLetter,out repeatIndx);
+               
+                if((i-repeatIndx)==1||(i-repeatIndx)==2)
+                {
+                    if(len<=(i-repeatIndx))
+                    {
+                        len=i-repeatIndx+1;
+                        startIndx=repeatIndx;
+                    }
+                    //左右扩展 
+                    int cirCount=repeatIndx>(s.Length-i)?(s.Length-i):repeatIndx;
+
+                    for(int g=1;g<=cirCount;g++)   //从0开始 判断自身是否相等
+                    {
+                        if (repeatIndx - g > -1 && (i + g) < s.Length)
+                        {
+                            if (s.Substring(repeatIndx - g, 1) == s.Substring(i + g, 1))
+                            {
+                                if ((i + 2 * g - repeatIndx) >= len)
+                                {
+                                    len = i + 2 * g - repeatIndx + 1;
+                                    startIndx = repeatIndx - g;
+                                }
+                            }
+                            else
+                                break;
+                        }
+                    }
+
+                    //向右扩展
+                    for(int k=i+1;k<s.Length;k++)
+                    {
+                        if (curretnLetter == s.Substring(k, 1))
+                        {
+                            if (k - repeatIndx >= len)
+                            {
+                                len = k - repeatIndx + 1;
+                                startIndx = repeatIndx;
+                            }
+                        }
+                        else
+                            break;
+                    }
+
+                }
+              
+                letterDic.Remove(curretnLetter);
+                letterDic.Add(curretnLetter,i);             
+            }
+            else
+                letterDic.Add(curretnLetter,i);    
+            
+        }
+
+
+        return s.Substring(startIndx,len);
     }
 }
